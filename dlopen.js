6afac7b012fd520009302cb8757562200afc2684
Alexey Orlenko <alex@aqrln.net>
2023-04-03 23:31:42 +0200

    Initial commit

---
+++ dlopen.js
@@ -0,0 +1,4 @@
+const addon = { exports: {} };
+
+process.dlopen(addon, "query_engine-windows.dll.node");
+process.dlopen(addon, "query_engine-windows.dll.node");
---
+++ require.js
@@ -0,0 +1,4 @@
+const path = require("node:path");
+
+require("./query_engine-windows.dll.node");
+require(path.toNamespacedPath("./query_engine-windows.dll.node"));
